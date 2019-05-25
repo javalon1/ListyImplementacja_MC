99dd9fc51afbc6c62c0bf61b534eca411989bd61
ciechann <emgasuk@gmail.com>
2019-05-25 12:16:49 +0100

    zadanie1

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,11 +1,43 @@
 package com.sda.algorytmy;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
+////        Box<Integer> box1 = new Box(new Integer(12)); // przekazujemy argument boxing
+////        Box<Float> box2 = new Box(13.32);// przekazujemy argument unboxing
+//
+//        List<Integer> integerList = new ArrayList<>();
+//        integerList.add(21);
+//        integerList.add(22341);
+//        integerList.add(2156233);
+//        integerList.add(2122);
+//        integerList.add(245639283);
+//
+//        for(Integer element: integerList){ //petla for each
+//            System.out.println(element);
+//        }
+//        for(int i=0; i<integerList.size();i++){  // petla for
+//            System.out.println(integerList.get(i));
+//        }
+//
+//        List<String> stringList = new ArrayList<>();
+//
+//        stringList.add("Tola");
+//        stringList.add("Bolek");
+//        stringList.add("Lolek");
+//
+//        for(String el: stringList){
+//            System.out.println(el);
+//        }
 
+//        String napis = "12345";
+//        int liczba = Integer.parseInt(napis); // konwersja stringa na liczbe
+//
 
-
+        Zadanie.zad1();
     }
 }
---
+++ src/main/java/com/sda/algorytmy/Zadanie.java
@@ -0,0 +1,30 @@
+package com.sda.algorytmy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Zadanie {
+
+
+    public static void zad1() {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj imie: ");
+        List<String> listaImion = new ArrayList<>();
+
+        while (true) {
+            String imie = scan.next();
+
+
+            if (imie.equals("-")) {
+                for (String el : listaImion) {
+                    System.out.println(el);
+                }
+                break;
+            }
+            listaImion.add(imie);
+        }
+    }
+}
